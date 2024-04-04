905fd78f3e6909fe019c3e42dac59cef4b637d95
Francisco Gutierrez <fco_gtz97@hotmail.com>
2024-04-04 17:50:46 -0600

    modiffied structure with ExpressJS

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-//import axios from 'axios';
 
+const express = require('express');
 const axios = require('axios');
+
+const app = express();
+const port = 3000;
 
 async function getUserData(userId) {
   try {
-    const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${userId}`
-    );
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching user data: ", error);
@@ -14,11 +15,17 @@ async function getUserData(userId) {
   }
 }
 
-const userId = 1;
-getUserData(userId)
-  .then((userData) => {
-    console.log("User Data: ", userData);
-  })
-  .catch((error) => {
-    console.error("Error: ", error);
-  });
+app.get('/users/:userId', async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const userData = await getUserData(userId);
+    res.json(userData);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching user data' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
